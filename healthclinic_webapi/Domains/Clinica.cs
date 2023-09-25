84ebecbbd5dbcc162a3bad3b9c3842325839f516
Luiz <luiz.henrique31415@gmail.com>
2023-09-25 10:53:06 -0300

    comecei API

---
+++ healthclinic_webapi/Domains/Clinica.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace healthclinic_webapi.Domains
+{
+    public class Clinica
+    {
+        [Key]
+
+        public Guid IdClinica { get; set; } = Guid.NewGuid();
+
+
+    }
+}
---
+++ healthclinic_webapi/Domains/Especialidade.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace healthclinic_webapi.Domains
+{
+    [Table(nameof(Especialidade))]
+    public class Especialidade
+    {
+        [Key]
+        public Guid IdEspecialidade { get; set; } = Guid.NewGuid();
+
+        [Column(TypeName = "VARCHAR (300)")]
+        [Required(ErrorMessage = "Especialidade obrigatoria")]
+        public string EspecialidadeNome { get; set; }
+    }
+}
